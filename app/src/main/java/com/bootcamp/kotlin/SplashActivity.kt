5249c0251843b7450f1e85151b09a4549cedc17a
jhonjto <jhon1985t@gmail.com>
2020-03-14 16:43:04 -0500

    SPLASH ACTIVITY # create splash activity + fragment

---
+++ app/src/main/java/com/bootcamp/kotlin/SplashActivity.kt
@@ -0,0 +1,18 @@
+package com.bootcamp.kotlin
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+
+import kotlinx.android.synthetic.main.activity_splash.*
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        
+    }
+
+}
