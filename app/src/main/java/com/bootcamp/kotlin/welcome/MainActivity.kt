043e923f0bff7fea4eefa4741a60d77b35b3d21c
DevEd20m <deved20m@gmail.com>
2020-03-18 00:26:28 -0500

    Se implementó 2 fragmentos más, el HomeFragment y el FavoriteFragment

--- app/src/main/java/com/bootcamp/kotlin/welcome/MainActivity.kt
+++ app/src/main/java/com/bootcamp/kotlin/welcome/MainActivity.kt
@@ -1,10 +1,17 @@
 package com.bootcamp.kotlin.welcome
 
+import android.content.Context
 import android.os.Bundle
+import android.util.AttributeSet
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import com.bootcamp.kotlin.R
+import com.bootcamp.kotlin.favorites.FavoriteFragment
+import com.bootcamp.kotlin.home.HomeFragment
 import com.bootcamp.kotlin.movies.Movies
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.custom_bottom_navigation.*
 import kotlinx.coroutines.*
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
@@ -14,7 +21,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        events()
         updateTextView("Hola Kotlin")
 
         val retrofit = Retrofit.Builder()
@@ -26,7 +33,16 @@ class MainActivity : AppCompatActivity() {
             val token = tokenAsync(retrofit)
             val movies = moviesAsync(retrofit)
 
-            updateTextView("${token.await().requestToken} Total de peliculas ${movies.await().results.size}")
+//            updateTextView("${token.await().requestToken} Total de peliculas ${movies.await().results.size}")
+        }
+    }
+
+    private fun events() {
+        ll_initHome.setOnClickListener {
+            changeFragment(HomeFragment.newInstance())
+        }
+        ll_initFavorites.setOnClickListener {
+            changeFragment(FavoriteFragment.newInstance())
         }
     }
 
@@ -46,5 +62,12 @@ class MainActivity : AppCompatActivity() {
     private suspend fun tokenAsync(retrofit: Retrofit) = CoroutineScope(Dispatchers.IO).async {
         val api = retrofit.create(Authorization::class.java)
         api.requestToken(apiKey = "5de961ca47ac20a3689205becc3c3b20")
+    }
+
+    private fun changeFragment(fragment:Fragment){
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.fl_main,fragment)
+            addToBackStack(null)
+        }.commit()
     }
 }
