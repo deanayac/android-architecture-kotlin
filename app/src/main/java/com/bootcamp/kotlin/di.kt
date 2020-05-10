c7cbf53543cfd6c6fd9eb4bb7244df2c21cca96c
Jhon Jarol Tabares Orozco <jhon1985t@gmail.com>
2020-05-10 14:00:22 -0500

    AGREGAR KOIN - CONFIGURACION INICIAL
    # añadir koin para trabajar con injeccion de dependencias

---
+++ app/src/main/java/com/bootcamp/kotlin/di.kt
@@ -0,0 +1,27 @@
+package com.bootcamp.kotlin
+
+import android.app.Application
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+import org.koin.dsl.module
+
+fun Application.initDI() {
+    startKoin {
+        androidLogger()
+        androidContext(this@initDI)
+        modules(listOf(appModule, dataModule, scopesModule))
+    }
+}
+
+private val appModule = module {
+
+}
+
+val dataModule = module {
+
+}
+
+private val scopesModule = module {
+
+}
