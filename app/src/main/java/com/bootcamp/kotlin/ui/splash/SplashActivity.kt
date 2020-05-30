aa68ffa70f5051e9f004094bcf044b9ddf26d227
jhon1985t <jhon1985t@gmail.com>
2020-05-30 14:18:33 -0500

    # update splash activity to viewmodel

--- app/src/main/java/com/bootcamp/kotlin/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/bootcamp/kotlin/ui/splash/SplashActivity.kt
@@ -1,62 +1,42 @@
 package com.bootcamp.kotlin.ui.splash
 
 import android.os.Bundle
-import android.os.Handler
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import com.bootcamp.kotlin.R
+import androidx.lifecycle.ViewModelProviders
 import com.bootcamp.kotlin.databinding.ActivitySplashBinding
 import com.bootcamp.kotlin.ui.main.MainActivity
 import com.bootcamp.kotlin.ui.register.RegisterFragment
-import com.bootcamp.kotlin.util.AccountRepositoryImpl
-import com.bootcamp.kotlin.util.attachFragment
-import com.bootcamp.kotlin.util.showMessage
 import com.bootcamp.kotlin.util.startActivity
 
-class SplashActivity : AppCompatActivity(), SplashContract.View, RegisterFragment.ActionListener {
+class SplashActivity : AppCompatActivity(), RegisterFragment.ActionListener {
 
-    private var presenter: SplashPresenter? = null
-    private val handler: Handler? = null
+    private lateinit var viewModel: SplashViewModel
     private lateinit var binding: ActivitySplashBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         binding = ActivitySplashBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        presenter = SplashPresenter(this, AccountRepositoryImpl(this), handler)
-        presenter?.initView()
+
+        viewModel = ViewModelProviders.of(this)[SplashViewModel::class.java]
 
         sleepScreen()
     }
 
+    private fun updateUi(model: SplashViewModel.UiModel) {
+        when (model) {
+            is SplashViewModel.UiModel.Navigation ->  navigateToHome()
+            is SplashViewModel.UiModel.SleepScreen -> sleepScreen()
+        }
+    }
+
     private fun sleepScreen() {
-        presenter?.sleepScreen()
+        viewModel.sleepScreen()
     }
 
     override fun navigateToHome() {
         startActivity<MainActivity>()
         finish()
-    }
-
-    override fun hideSplash() {
-        binding.containerSplash.visibility = View.GONE
-    }
-
-    override fun onDestroy() {
-        presenter?.onDestroy()
-        super.onDestroy()
-    }
-
-    override fun showMessage(message: String) {
-        applicationContext.showMessage(message)
-    }
-
-    override fun showRegister() {
-        attachFragment(
-            R.id.splashContainer,
-            RegisterFragment(),
-            getString(R.string.tag_register_fragment)
-        )
-        hideSplash()
     }
 }
---
+++ app/src/main/java/com/bootcamp/kotlin/ui/splash/SplashViewModel.kt
@@ -0,0 +1,51 @@
+package com.bootcamp.kotlin.ui.splash
+
+import android.os.Handler
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.bootcamp.kotlin.ui.common.ScopedViewModel
+import com.bootcamp.kotlin.util.AccountRepository
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.launch
+
+/**
+ * Created by jhon on 6/04/2020
+ */
+class SplashViewModel(
+    private val repository: AccountRepository,
+    uiDispatcher: CoroutineDispatcher
+): ScopedViewModel(uiDispatcher) {
+
+    private val _model = MutableLiveData<UiModel>()
+    val model: LiveData<UiModel>
+    get() {
+        if (_model.value == null) sleepScreen()
+        return _model
+    }
+
+    sealed class UiModel {
+        object SleepScreen : UiModel()
+        object Navigation : UiModel()
+    }
+
+    init {
+        initScope()
+    }
+
+    fun sleepScreen() {
+        launch {
+            repository.checkIfUserExists().apply {
+                if (isEmpty()) {
+                    _model.value = UiModel.Navigation
+                    return@apply
+                }
+                _model.value = UiModel.SleepScreen
+            }
+        }
+    }
+
+    override fun onCleared() {
+        destroyScope()
+        super.onCleared()
+    }
+}
